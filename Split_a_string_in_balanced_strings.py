0875a65a65d8bf970c4ee4803047b14f9240b8f0
Jaime Saraza <70734810+jsaraza@users.noreply.github.com>
2023-10-07 09:28:46 -0700

    Solved #1221 Split a String in a Balanced String

---
+++ Split_a_string_in_balanced_strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        stack, result = [], 0 
+
+        for char in s:
+            if stack == []:
+                stack.append(char)
+                result += 1
+            elif char == stack[-1]:
+                stack.append(char)
+            else:
+                stack.pop()
+        
+        return result
