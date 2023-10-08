3e5f56d5449afa1167e615bf3f057be7c728e7a8
Jaime Saraza <70734810+jsaraza@users.noreply.github.com>
2023-10-08 14:56:33 -0700

    Solved #13 Roman to Integer

---
+++ Roman_to_integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        m = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]] :
+                res -= m[s[i]]
+            else:
+                res += m[s[i]]
+
+        return res
